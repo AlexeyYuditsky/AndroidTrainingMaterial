b2c0b72cdda35f8b54f62b74c03ebba31819b821
Alexey Yuditsky <alexeyyuditsky@gmail.com>
2022-03-02 19:28:02 +0700

    Minor edits

---
+++ mvvm-foundation-coroutines/app/src/main/java/ua/cn/stu/foundation/uiactions/UIActions.kt
@@ -0,0 +1,14 @@
+package ua.cn.stu.foundation.uiactions
+
+import androidx.annotation.StringRes
+
+/** Common actions that can be performed in the view-model */
+interface UIActions {
+
+    /** Display a simple toast message. */
+    fun toast(message: String)
+
+    /** Get string resource content by its identifier. */
+    fun getString(@StringRes messageRes: Int, vararg args: Any): String
+
+}
