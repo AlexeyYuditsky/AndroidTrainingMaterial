73d428c1c1ca7c2f19fb5677d0276e7619f9a4a3
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-04-12 13:40:55 +0700

    Final work with screen Profile and EditProfile

--- navigation-component-hard-example-mvvm/app/src/main/java/com/alexeyyuditsky/test/screens/main/tabs/profile/EditProfileFragment.kt
+++ navigation-component-hard-example-mvvm/app/src/main/java/com/alexeyyuditsky/test/screens/main/tabs/profile/EditProfileFragment.kt
@@ -2,43 +2,70 @@ package com.alexeyyuditsky.test.screens.main.tabs.profile
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import com.alexeyyuditsky.test.R
+import com.alexeyyuditsky.test.Repositories
 import com.alexeyyuditsky.test.databinding.FragmentEditProfileBinding
+import com.alexeyyuditsky.test.utils.viewModelCreator
 
 class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
 
     private lateinit var binding: FragmentEditProfileBinding
 
+    private val viewModel by viewModelCreator { EditProfileViewModel(Repositories.accountsRepository) }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentEditProfileBinding.bind(view)
+
         binding.cancelButton.setOnClickListener { onCancelButtonPressed() }
         binding.saveButton.setOnClickListener { onSaveButtonPressed() }
 
-        if (savedInstanceState == null) {
-            binding.usernameEditText.setText(getUsername())
+        if (savedInstanceState == null) observeInitialUsernameEvent()
+        observeShowInProgress()
+        observeEmptyFieldErrorEvent()
+        observeGoBack()
+    }
+
+    private fun observeInitialUsernameEvent() = viewModel.initialUsernameEvent.observe(viewLifecycleOwner) {
+        it.get()?.let { account ->
+            binding.usernameEditText.setText(account.username)
         }
     }
 
-    private fun getUsername(): String {
-        val arg by navArgs<EditProfileFragmentArgs>()
-        return arg.username
+    private fun observeShowInProgress() = viewModel.saveInProgress.observe(viewLifecycleOwner) {
+        if (it) {
+            binding.progressBar.isVisible = true
+            binding.usernameTextInput.isEnabled = false
+            binding.saveButton.isEnabled = false
+        } else {
+            binding.progressBar.isVisible = false
+            binding.usernameTextInput.isEnabled = true
+            binding.saveButton.isEnabled = true
+        }
     }
 
-    private fun onSaveButtonPressed() {
-        requireActivity().supportFragmentManager.setFragmentResult(
-            ProfileFragment.EXTRA_USERNAME,
-            bundleOf(ProfileFragment.EXTRA_USERNAME to binding.usernameEditText.text.toString())
-        )
-        findNavController().popBackStack()
+    private fun observeEmptyFieldErrorEvent() = viewModel.emptyFieldErrorEvent.observe(viewLifecycleOwner) {
+        it.get()?.let {
+            Toast.makeText(requireContext(), R.string.field_is_empty, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun observeGoBack() = viewModel.goBack.observe(viewLifecycleOwner) {
+        it.get()?.let {
+            findNavController().popBackStack()
+        }
     }
 
     private fun onCancelButtonPressed() {
         findNavController().popBackStack()
+    }
+
+    private fun onSaveButtonPressed() {
+        viewModel.saveUsername(binding.usernameEditText.text.toString())
     }
 
 }
--- navigation-component-hard-example-mvvm/app/src/main/java/com/alexeyyuditsky/test/screens/main/tabs/profile/ProfileFragment.kt
+++ navigation-component-hard-example-mvvm/app/src/main/java/com/alexeyyuditsky/test/screens/main/tabs/profile/ProfileFragment.kt
@@ -3,48 +3,43 @@ package com.alexeyyuditsky.test.screens.main.tabs.profile
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
-import com.alexeyyuditsky.test.MainActivity
+import androidx.navigation.fragment.findNavController
 import com.alexeyyuditsky.test.R
+import com.alexeyyuditsky.test.Repositories
 import com.alexeyyuditsky.test.databinding.FragmentProfileBinding
-import com.alexeyyuditsky.test.screens.main.tabs.TabsFragmentDirections
+import com.alexeyyuditsky.test.utils.findTopNavController
+import com.alexeyyuditsky.test.utils.viewModelCreator
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private lateinit var binding: FragmentProfileBinding
 
+    private val viewModel by viewModelCreator { ProfileViewModel(Repositories.accountsRepository) }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProfileBinding.bind(view)
+
         binding.editProfileButton.setOnClickListener { onEditProfileButtonPressed() }
         binding.logoutButton.setOnClickListener { onLogoutButtonPressed() }
 
-        requireActivity().supportFragmentManager.setFragmentResultListener(
-            EXTRA_USERNAME,
-            viewLifecycleOwner
-        ) { _, result ->
-            binding.userNameTextView.text = result.getString(EXTRA_USERNAME)
-        }
+        observeAccountDetails()
+    }
+
+    private fun observeAccountDetails() = viewModel.account.observe(viewLifecycleOwner) { account ->
+        if (account == null) return@observe
+        binding.emailTextView.text = account.email
+        binding.userNameTextView.text = account.username
+        binding.createdAtTextView.text = account.createdAt
     }
 
     private fun onLogoutButtonPressed() {
-        val topLevelHost =
-            requireActivity().supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment?
-        //topLevelHost?.navController?.navigate(R.id.action_tabsFragment_to_signInFragment)
-        (requireActivity() as MainActivity).toolbarTextViewListener("")
+        viewModel.logout()
+        findTopNavController().navigate(R.id.action_tabsFragment_to_signInFragment)
     }
 
     private fun onEditProfileButtonPressed() {
-        val arg = binding.userNameTextView.text.toString()
-        val direction = TabsFragmentDirections.actionTabsFragmentToEditProfileFragment(arg)
-
-        val topLevelHost =
-            requireActivity().supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment?
-        topLevelHost?.navController?.navigate(direction)
-    }
-
-    companion object {
-        const val EXTRA_USERNAME = "username"
+        findTopNavController().navigate(R.id.editProfileFragment)
     }
 
 }
