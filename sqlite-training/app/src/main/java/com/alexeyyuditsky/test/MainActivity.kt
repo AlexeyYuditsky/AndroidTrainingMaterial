282f3291c1634a3344e3c481708bfd244a7ba09e
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-04-28 11:00:48 +0700

    SQLite training project

---
+++ sqlite-training/app/src/main/java/com/alexeyyuditsky/test/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.alexeyyuditsky.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexeyyuditsky.test.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+    }
+
+}
