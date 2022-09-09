dc6739d3190b172b4b3d49b55f7475ac512d5f10
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-09-09 12:31:19 +0700

    Impl ViewModel factory

--- clean-architecture/app/src/main/java/com/alexeyyuditsky/test/presentation/MainActivity.kt
+++ clean-architecture/app/src/main/java/com/alexeyyuditsky/test/presentation/MainActivity.kt
@@ -1,37 +1,47 @@
 package com.alexeyyuditsky.test.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.alexeyyuditsky.data.repository.UserRepositoryImpl
 import com.alexeyyuditsky.data.storage.sharedpref.SharedPrefUserStorage
 import com.alexeyyuditsky.test.databinding.ActivityMainBinding
 import com.alexeyyuditsky.domain.model.UserNameParam
 import com.alexeyyuditsky.domain.usecase.GetUserNameUseCase
 import com.alexeyyuditsky.domain.usecase.SaveUserNameUseCase
+import com.alexeyyuditsky.test.log
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private val userStorage by lazy((LazyThreadSafetyMode.NONE)) { SharedPrefUserStorage(applicationContext) }
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) { UserRepositoryImpl(userStorage) }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) { GetUserNameUseCase(userRepository) }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) { SaveUserNameUseCase(userRepository) }
+    private val viewModel by lazy {
+        ViewModelProvider(this, MainViewModelFactory(applicationContext))[MainViewModel::class.java]
+    }
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        lifecycleScope.launch {
+            viewModel.nameFlow.collectLatest {
+                binding.textView.text = it
+            }
+        }
+
         binding.getNameButton.setOnClickListener {
-            val userName = getUserNameUseCase.execute()
-            binding.textView.text = "$userName"
+            viewModel.load()
         }
 
         binding.saveNameButton.setOnClickListener {
             val name = binding.editText.text.toString()
-            val userNameParam = UserNameParam(name = name)
-            val result = saveUserNameUseCase.execute(userNameParam)
-            binding.textView.text = "Save result = $result"
+            viewModel.save(name)
         }
     }
 
