71cc0a8c66ad0ca8ef3b2d34fc8a85f8687b49ce
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-10-21 11:22:17 +0700

    Create ConstraintActivity

--- compose/app/src/main/java/com/alexeyyuditsky/compose/ListActivity.kt
+++ compose/app/src/main/java/com/alexeyyuditsky/compose/ListActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+// part 8
 class ListActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
