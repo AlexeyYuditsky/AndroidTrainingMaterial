450982cd5653324e4b99c54a0bb69e62e1294982
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-01-13 19:40:21 +0700

    Add task 2 for receiver

--- broadcast-receiver/app/src/main/java/com/alexeyyuditsky/test/CustomReceiver.kt
+++ broadcast-receiver/app/src/main/java/com/alexeyyuditsky/test/CustomReceiver.kt
@@ -11,10 +11,18 @@ class CustomReceiver : BroadcastReceiver() {
         val toastMessage = when (intent?.action) {
             Intent.ACTION_POWER_CONNECTED -> "Power connected!"
             Intent.ACTION_POWER_DISCONNECTED -> "Power disconnected!"
+            ACTION_CUSTOM_BROADCAST -> {
+                val randomValue = intent.getIntExtra("random", 0)
+                "Custom Broadcast Received\nRandom value: $randomValue"
+            }
             else -> "unknown intent action"
         }
 
         Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        const val ACTION_CUSTOM_BROADCAST = "${BuildConfig.APPLICATION_ID}.ACTION_CUSTOM_BROADCAST"
     }
 
 }
--- broadcast-receiver/app/src/main/java/com/alexeyyuditsky/test/MainActivity.kt
+++ broadcast-receiver/app/src/main/java/com/alexeyyuditsky/test/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.alexeyyuditsky.test.CustomReceiver.Companion.ACTION_CUSTOM_BROADCAST
 import com.alexeyyuditsky.test.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,17 +19,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val filter = IntentFilter().apply {
-            addAction(Intent.ACTION_POWER_CONNECTED)
-            addAction(Intent.ACTION_POWER_DISCONNECTED)
-        }
-
-        registerReceiver(mReceiver, filter)
+        registerPowerReceiver()
+        registerLocaleReceiver()
     }
 
     override fun onDestroy() {
         unregisterReceiver(mReceiver)
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver)
         super.onDestroy()
+    }
+
+    private fun registerPowerReceiver() {
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(mReceiver, filter)
+    }
+
+    private fun registerLocaleReceiver() {
+        binding.sendBroadcast.setOnClickListener {
+            val customBroadcastIntent = Intent(ACTION_CUSTOM_BROADCAST)
+            customBroadcastIntent.putExtra("random", Random.nextInt(1, 20))
+            LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent)
+        }
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, IntentFilter(ACTION_CUSTOM_BROADCAST))
     }
 
 }
