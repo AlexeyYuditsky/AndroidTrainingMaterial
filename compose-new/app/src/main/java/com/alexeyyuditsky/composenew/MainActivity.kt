ab537e41df1f6140f20991dcbf09e29d310b8899
AYuditskiy <Alexey.Yuditskiy@cse.ru>
2023-09-05 08:44:58 +0300

    Init 1

--- compose-new/app/src/main/java/com/alexeyyuditsky/composenew/MainActivity.kt
+++ compose-new/app/src/main/java/com/alexeyyuditsky/composenew/MainActivity.kt
@@ -3,44 +3,37 @@ package com.alexeyyuditsky.composenew
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.alexeyyuditsky.composenew.ui.theme.ComposenewTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposenewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            UserInfoPreview()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UserInfoPreview() {
+    UserInfo(name = "John", age = 28)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposenewTheme {
-        Greeting("Android")
+fun UserInfo(name: String, age: Int) {
+    Column {
+        Text(
+            text = "Привет $name тебе $age",
+            color = Color.Red
+        )
+        Text(
+            text = "Привет ",
+            color = Color.Green
+        )
     }
 }
