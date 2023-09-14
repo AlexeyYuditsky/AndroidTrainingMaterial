a4a78194a82857d40bfaa25b9f0fd3d253a5bf56
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-09-14 08:58:23 +0300

    InstagramProfileCard 2

--- compose-new/app/src/main/java/com/alexeyyuditsky/composenew/MainActivity.kt
+++ compose-new/app/src/main/java/com/alexeyyuditsky/composenew/MainActivity.kt
@@ -3,6 +3,17 @@ package com.alexeyyuditsky.composenew
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alexeyyuditsky.composenew.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
@@ -11,5 +22,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             InstagramProfileCard()
         }
+    }
+}
+
+@Preview
+@Composable
+fun CardTest() {
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp,
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.White
+        ),
+        border = BorderStroke(1.dp, Color.Red)
+    ) {
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = "Hello world!"
+        )
     }
 }
--- compose-new/app/src/main/java/com/alexeyyuditsky/composenew/ui/theme/InstagramProfileCard.kt
+++ compose-new/app/src/main/java/com/alexeyyuditsky/composenew/ui/theme/InstagramProfileCard.kt
@@ -1,5 +1,6 @@
 package com.alexeyyuditsky.composenew.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,6 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,50 +24,63 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
+    Card(
         modifier = Modifier
-            .fillMaxWidth()
             .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+        shape = RoundedCornerShape(
+            topStart = 8.dp,
+            topEnd = 8.dp
+        ),
+        border = BorderStroke(
+            width = 1.dp,
+            color = Color.Black
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(Color.Green)
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .background(Color.Yellow)
+            )
 
+            TwoBox("6950", "Posts")
+            TwoBox("436M", "Followers")
+            TwoBox("76", "Following")
         }
-
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
     }
 }
 
-@Preview
 @Composable
-private fun TwoBoxes() {
+private fun TwoBox(quantity: String, name: String) {
     Column(
-        modifier = Modifier
-            .height(100.dp)
-            .background(Color.Red),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
+        modifier = Modifier.height(100.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
             modifier = Modifier
-                .size(25.dp)
-                .background(Color.Green)
+                .background(Color.Red)
+                .size(40.dp),
+            contentAlignment = Alignment.Center
         ) {
-
+            Text(text = quantity)
         }
         Box(
             modifier = Modifier
-                .size(25.dp)
                 .background(Color.Blue)
+                .size(40.dp),
+            contentAlignment = Alignment.Center
         ) {
-
+            Text(text = name)
         }
     }
 }
