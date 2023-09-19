014bd64e0a304b6361ee517c63ac663e67d24051
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-09-19 09:01:55 +0300

    InstagramProfileCard 3

--- compose-new/app/src/main/java/com/alexeyyuditsky/composenew/MainActivity.kt
+++ compose-new/app/src/main/java/com/alexeyyuditsky/composenew/MainActivity.kt
@@ -3,45 +3,44 @@ package com.alexeyyuditsky.composenew
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.alexeyyuditsky.composenew.ui.theme.ComposeNewTheme
 import com.alexeyyuditsky.composenew.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramProfileCard()
+            ComposeNewTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            color = MaterialTheme.colorScheme.background
+                        )
+                )
+                InstagramProfileCard()
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun CardTest() {
-    Card(
-        shape = RoundedCornerShape(
-            topStart = 4.dp,
-            topEnd = 4.dp,
-        ),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.Yellow,
-            contentColor = Color.White
-        ),
-        border = BorderStroke(1.dp, Color.Red)
-    ) {
-        Text(
-            modifier = Modifier.padding(4.dp),
-            text = "Hello world!"
-        )
-    }
+fun TestImage() {
+    Image(
+        modifier = Modifier.fillMaxSize(),
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "",
+        contentScale = ContentScale.FillHeight
+    )
 }
--- compose-new/app/src/main/java/com/alexeyyuditsky/composenew/ui/theme/InstagramProfileCard.kt
+++ compose-new/app/src/main/java/com/alexeyyuditsky/composenew/ui/theme/InstagramProfileCard.kt
@@ -11,15 +11,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
@@ -33,16 +39,16 @@ fun InstagramProfileCard() {
         ),
         border = BorderStroke(
             width = 1.dp,
-            color = Color.Black
+            color = MaterialTheme.colorScheme.onBackground
         ),
         colors = CardDefaults.cardColors(
-            containerColor = Color.White
+            containerColor = MaterialTheme.colorScheme.background
         )
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(8.dp),
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically,
         ) {
@@ -52,35 +58,67 @@ fun InstagramProfileCard() {
                     .background(Color.Yellow)
             )
 
-            TwoBox("6950", "Posts")
-            TwoBox("436M", "Followers")
-            TwoBox("76", "Following")
+            UserStatistics("Posts", "6,950")
+            UserStatistics("Followers", "436M")
+            UserStatistics("Following", "76")
+        }
+
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Instagram")
+            Text(text = "#YoursToMake")
+            Text(text = "www.facebook.com/emotional_health")
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RectangleShape
+            ) {
+                Text(text = "Follow")
+            }
         }
     }
 }
 
 @Composable
-private fun TwoBox(quantity: String, name: String) {
+private fun UserStatistics(
+    tittle: String,
+    value: String
+) {
     Column(
         modifier = Modifier.height(100.dp),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
-            modifier = Modifier
-                .background(Color.Red)
-                .size(40.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = quantity)
-        }
-        Box(
-            modifier = Modifier
-                .background(Color.Blue)
-                .size(40.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = name)
-        }
+        Text(
+            text = value,
+            fontSize = 24.sp,
+            fontFamily = FontFamily.Cursive,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(
+            text = tittle,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardLight() {
+    ComposeNewTheme(
+        darkTheme = false
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark() {
+    ComposeNewTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
     }
 }
