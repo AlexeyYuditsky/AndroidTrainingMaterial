35c299672fd3357d5f92d819ac11def65f6aa808
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-12-28 13:24:25 +0300

    Compose

--- compose-new/vkclient/src/main/java/com/alexeyyuditsky/vkclient/ui/theme/NavigationItem.kt
+++ compose-new/vkclient/src/main/java/com/alexeyyuditsky/vkclient/ui/theme/NavigationItem.kt
@@ -7,27 +7,28 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.alexeyyuditsky.vkclient.R
+import com.alexeyyuditsky.vkclient.navigation.Screen
 
 sealed class NavigationItem(
+    val screen: Screen,
     @StringRes val id: Int,
     val icon: ImageVector
 ) {
     object Home : NavigationItem(
+        screen = Screen.NewsFeed,
         id = R.string.navigation_item_main,
         icon = Icons.Outlined.Home
     )
 
     object Favorite : NavigationItem(
+        screen = Screen.Favourite,
         id = R.string.navigation_item_favorite,
         icon = Icons.Outlined.Favorite
     )
 
     object Profile : NavigationItem(
+        screen = Screen.Profile,
         id = R.string.navigation_item_profile,
         icon = Icons.Outlined.Person
     )
-}
-
-fun main() {
-    val a = NavigationItem.Favorite as NavigationItem
 }
--- compose-new/vkclient/src/main/java/com/alexeyyuditsky/vkclient/ui/theme/VkNewsMainScreen.kt
+++ compose-new/vkclient/src/main/java/com/alexeyyuditsky/vkclient/ui/theme/VkNewsMainScreen.kt
@@ -1,6 +1,5 @@
 package com.alexeyyuditsky.vkclient.ui.theme
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -11,34 +10,39 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.alexeyyuditsky.vkclient.MainViewModel
+import com.alexeyyuditsky.vkclient.navigation.AppNavGraph
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(
     viewModel: MainViewModel
 ) {
-    val selectedNavItem by viewModel.selectedNavItem.observeAsState(NavigationItem.Home)
+    val navHostController = rememberNavController()
 
     Scaffold(
         bottomBar = {
             NavigationBar {
+                val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
                 val items = arrayOf(
                     NavigationItem.Home,
                     NavigationItem.Favorite,
                     NavigationItem.Profile
                 )
+
                 items.forEach { item ->
                     NavigationBarItem(
-                        selected = selectedNavItem == item,
-                        onClick = { viewModel.selectNavItem(item) },
+                        selected = currentRoute == item.screen.route,
+                        onClick = { navHostController.navigate(item.screen.route) },
                         icon = { Icon(imageVector = item.icon, contentDescription = null) },
                         label = { Text(text = stringResource(id = item.id)) },
                         colors = NavigationBarItemDefaults.colors(
@@ -50,26 +54,28 @@ fun MainScreen(
             }
         }
     ) { paddingValues ->
-        var stateFavorite by remember { mutableIntStateOf(0) }
-        var stateProfile by remember { mutableIntStateOf(0) }
-
-        when (selectedNavItem) {
-            is NavigationItem.Home -> HomeScreen(
-                viewModel = viewModel,
-                paddingValues = paddingValues
-            )
-
-            is NavigationItem.Favorite -> TextCounter(name = "Favorite", stateFavorite) { stateFavorite++ }
-            is NavigationItem.Profile -> TextCounter(name = "Profile", stateProfile) { stateProfile++ }
-        }
+        AppNavGraph(
+            navHostController = navHostController,
+            homeScreenContent = {
+                HomeScreen(
+                    viewModel = viewModel,
+                    paddingValues = paddingValues
+                )
+            },
+            favouriteScreenContent = { TextCounter(name = "Favourite") },
+            profileScreenContent = { TextCounter(name = "Profile") }
+        )
     }
 }
 
 @Composable
-fun TextCounter(name: String, state: Int, clickListener: () -> Int) {
+fun TextCounter(name: String) {
+    var count by remember {
+        mutableIntStateOf(0)
+    }
     Text(
-        modifier = Modifier.clickable { clickListener() },
-        text = "$name counter: $state",
+        modifier = Modifier.clickable { count++ },
+        text = "$name counter: $count",
         color = Color.Black
     )
 }
