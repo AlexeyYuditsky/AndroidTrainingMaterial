1f9887f8ca47a1f3a0c8ce711ab1204571001825
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-02-19 07:40:25 +0300

    create new repository compose

--- compose-new/app/src/main/java/com/alexeyyuditsky/composenew/InstagramModel.kt
+++
@@ -1,7 +0,0 @@
-package com.alexeyyuditsky.composenew
-
-data class InstagramModel(
-    val id: Int,
-    val title: String,
-    val isFollowed: Boolean
-)
--- compose-new/app/src/main/java/com/alexeyyuditsky/composenew/Log.kt
+++
@@ -1,7 +0,0 @@
-package com.alexeyyuditsky.composenew
-
-import android.util.Log
-
-fun <T> log(message: T) {
-    Log.d("MyLog", message.toString())
-}
